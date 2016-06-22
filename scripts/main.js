5cda66ec659ca6e0a3f26dde9a6c2a93879e2d25
Bogdan Matei <contact@bmatei.com>
2016-06-22 20:34:21 +0300

    Added Plex support

--- scripts/main.js
+++ scripts/main.js
@@ -5,37 +5,9 @@
         currentResource,
         addPipButtons,
         findVideos,
+        plexObserver,
+        plexObserverTrigger,
         initPiPTool;
-
-    /** @type {Array} An array with every platform and the custom options for them */
-    resources = [
-        {
-            name: 'dailymotion',
-            testPattern: /(dailymotion\.com|www\.dailymotion\.com)/,
-            customLoadEvent: null,
-            elementType: 'button',
-            videoSelector: 'video#dmp_Video',
-            buttonClassList: 'dmp_ControlBarButton pip-button',
-            videoParentClass: '.dmp_Player',
-            controlsWrapperClass: '.dmp_ControlBar'
-        },
-        {
-            name: 'youtube',
-            testPattern: /(youtube\.com|www\.youtube\.com|youtu\.be|www\.youtu\.be)/,
-            customLoadEvent: {
-                name: 'spfdone',
-                loaded: false
-            },
-            elementType: 'button',
-            videoSelector: 'video.html5-main-video',
-            buttonClassList: 'ytp-button pip-button',
-            videoParentClass: '.html5-video-player',
-            controlsWrapperClass: '.ytp-right-controls'
-        }
-    ];
-
-    /** @type {Object} An object keeping the current platform options */
-    currentResource = null;
 
     /**
      * Add the PiP event and button to a given video
@@ -92,16 +64,95 @@
         }
     };
 
+    /** The method used to listen and trigger the event of finding the videos */
+    plexObserver = function (mutations) {
+        mutations.forEach(function (mutation) {
+            var addedNodesIterator;
+
+            for (addedNodesIterator = 0; addedNodesIterator < mutation.addedNodes.length; addedNodesIterator++) {
+                if (mutation.addedNodes[addedNodesIterator].classList && mutation.addedNodes[addedNodesIterator].classList.contains(currentResource.customClasses.videoClassObserver)) {
+                    findVideos();
+                }
+            }
+        });
+    };
+
+    /** The trigger of the Plex Observer */
+    plexObserverTrigger = function () {
+        var observer;
+
+        /** @type {MutationObserver} Initialize an observer */
+        observer = new MutationObserver(plexObserver);
+
+        /** Set the observer */
+        observer.observe(document.querySelector(currentResource.customClasses.plexContainer), {
+            childList: true
+        });
+    };
+
     /** Method to trigger the PiP button display */
     initPiPTool = function () {
+        /** @type {Array} An array with every platform and the custom options for them */
+        resources = [
+            {
+                name: 'dailymotion',
+                testPattern: /(dailymotion\.com|www\.dailymotion\.com)/,
+                customLoadEvent: null,
+                elementType: 'button',
+                videoSelector: 'video#dmp_Video',
+                buttonClassList: 'dmp_ControlBarButton pip-button',
+                videoParentClass: '.dmp_Player',
+                controlsWrapperClass: '.dmp_ControlBar',
+                customClasses: null
+            },
+            {
+                name: 'plex',
+                testPattern: /(plex\.tv|www\.plex\.tv|app\.plex\.tv)/,
+                customLoadEvent: {
+                    name: 'DOMContentLoaded',
+                    method: plexObserverTrigger,
+                    loaded: false
+                },
+                elementType: 'button',
+                videoSelector: 'video.html-video',
+                buttonClassList: 'btn-link pip-button',
+                videoParentClass: '.video-container',
+                controlsWrapperClass: '.video-controls-overlay-bottom .video-controls-right',
+                customClasses: {
+                    plexContainer: '#plex',
+                    videoClassObserver: 'video-player'
+                }
+            },
+            {
+                name: 'youtube',
+                testPattern: /(youtube\.com|www\.youtube\.com|youtu\.be|www\.youtu\.be)/,
+                customLoadEvent: {
+                    name: 'spfdone',
+                    method: findVideos,
+                    loaded: false
+                },
+                elementType: 'button',
+                videoSelector: 'video.html5-main-video',
+                buttonClassList: 'ytp-button pip-button',
+                videoParentClass: '.html5-video-player',
+                controlsWrapperClass: '.ytp-right-controls',
+                customClasses: null
+            }
+        ];
+
+        /** @type {Object} An object keeping the current platform options */
+        currentResource = null;
+
         resources.forEach(function (resource) {
             if (location.hostname.match(resource.name)) {
                 currentResource = resource;
 
+                /** Add the event for normal pages */
                 window.addEventListener('DOMContentLoaded', findVideos, true);
 
+                /** Try to see if we have any custom handlers for this page (for instance DailyMotion). Usually these are used with SPAs (single page applications) like YouTube or Plex */
                 if (null !== currentResource.customLoadEvent && false === currentResource.customLoadEvent.loaded) {
-                    window.addEventListener(currentResource.customLoadEvent.name, findVideos);
+                    window.addEventListener(currentResource.customLoadEvent.name, currentResource.customLoadEvent.method, true);
 
                     currentResource.customLoadEvent.loaded = true;
                 }
